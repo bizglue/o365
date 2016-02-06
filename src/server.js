1cb827c3a8143858c50e209577c97f949e3b885a
Matt Dotson <matt@dotsons.org>
2016-02-06 14:49:26 -0800

    initial app

---
+++ src/server.js
@@ -0,0 +1,13 @@
+var express = require("express");
+
+var port = process.env.PORT | 1338;
+var app = express();
+
+app.get('/', function(req, res){
+	res.write('bizglue/o365');
+	res.end();
+});
+
+app.listen(port, function(){
+	console.log('Example app listening on port' + port);
+});
